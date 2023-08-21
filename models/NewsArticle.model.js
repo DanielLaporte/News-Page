610585fb4018b80310b4965d4010477c31f32116
DanielLaporte <daniel.laporte2008@gmail.com>
2023-08-21 21:32:15 +0200

    engtrega 3

--- models/NewsArticle.model.js
+++ models/NewsArticle.model.js
@@ -6,7 +6,7 @@ const newsArticleSchema = new Schema({
   content: { type: String, required: true },
   author: { type: String, required: true },
   section: { type: String, required: true },
-  imageLink: { type: String, required: true }, // Nuevo campo para el enlace de imagen
+  
 });
 
 const NewsArticle = mongoose.model('NewsArticle', newsArticleSchema);
